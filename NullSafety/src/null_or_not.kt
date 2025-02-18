b5026a0ca941e3b15df23dcb2d93ec9aa337e395
Njood <nejood03@gmail.com>
2025-02-18 18:23:38 +0300

    final step is done and scores have been added

--- NullSafety/src/null_or_not.kt
+++ NullSafety/src/null_or_not.kt
@@ -3,9 +3,19 @@ fun main() {
     println(age)
 
     var city: String? = "Kuwait City"
-    println("City length: ${city?.length?:0}")
+    println("City length: ${city?.length ?: 0}")
 
     city = null
+    println("City length: ${city?.length ?: 0}")
 
-    println("City length: ${city?.length?:0}")
+    val score: Double? = 92.0
+    printScore(score)
+
+    val nullScore: Double? = null
+    printScore(nullScore)
+}
+
+fun printScore(score: Double?) {
+    val finalScore = score ?: 0
+    print(finalScore)
 }
